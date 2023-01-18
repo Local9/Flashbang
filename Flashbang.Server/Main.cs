3f9295e2786dc7aa502caaf4e76b0852bcef8b32
local9 <apjcookie1@hotmail.com>
2023-01-18 20:06:04 +0000

    tweak(server): send to closest if OneSync is enabled

--- Flashbang.Server/Main.cs
+++ Flashbang.Server/Main.cs
@@ -13,7 +13,8 @@ namespace Flashbang.Server
     {
         private Config _config = new();
         private PlayerList _playerList;
-            
+        private bool _isOneSyncEnabled => API.GetConvar("onesync", "off") != "off";
+
         public Main()
         {
             _config.Load();
@@ -55,11 +56,18 @@ namespace Flashbang.Server
             flashbangMessage.Damage = _config.Damage;
             flashbangMessage.LethalRadius = _config.LethalRadius;
 
-            List<Player> closestPlayers = GetClosestPlayers(flashbangMessage.Position, _config.MaxUpdateRange);
-
-            foreach (Player player in closestPlayers)
+            if (_isOneSyncEnabled)
             {
-                TriggerClientEvent(player, "Flashbang:Explode", JsonConvert.SerializeObject(flashbangMessage));
+                List<Player> closestPlayers = GetClosestPlayers(flashbangMessage.Position, _config.MaxUpdateRange);
+
+                foreach (Player player in closestPlayers)
+                {
+                    TriggerClientEvent(player, "Flashbang:Explode", JsonConvert.SerializeObject(flashbangMessage));
+                }
+            }
+            else
+            {
+                TriggerClientEvent("Flashbang:Explode", JsonConvert.SerializeObject(flashbangMessage));
             }
         }
     }
